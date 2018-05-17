96f3b78fca537f180c77dccc245ba523fa760593
gijsje789 <gijsjaapdevries@hotmail.com>
2018-05-17 15:29:38 +0200

    Gabor attempt

--- fingervein_recognition.m
+++ fingervein_recognition.m
@@ -13,9 +13,26 @@ for ii=1:nfiles
 end
 
 %% Variables
+GaborSigma = 5;
+dF = 8;
+Gaborf0 = 0.25;%(sqrt( log(2/pi))*(2^dF + 1)/(2^dF - 1)) / GaborSigma;
+GaborK = 1/(2 * pi * GaborSigma^2);
 
 %% Processing
 % Show an image
 origIm = figure;
-subplot(1,2,1)
+subplot(2,1,1)
 imshow(images{1}, [])
+
+% % Binarize using a adaptive threshold
+% Thres = adaptthresh(images{1}, 0.6);
+% bin = imbinarize(images{1}, Thres);
+% 
+% subplot(1,2,2)
+% imshow(bin);
+x=-25:25;
+y=-25:25;
+g = GaborK * exp((-(x.^2 + transpose(y.^2))) / (2 * GaborSigma^2));
+Gabor = g * exp(2*pi * i * Gaborf0 * sqrt(x.^2 + transpose(y.^2)));
+subplot(2,1,2)
+surf(x,y,abs(Gabor));
