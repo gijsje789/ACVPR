cdf3c20d05236e5827fa47c93214b85d380f1c26
gijsje789 <gijsjaapdevries@hotmail.com>
2018-05-20 10:51:05 +0200

    Gabor attempt 2 with other paper

--- fingervein_recognition.m
+++ fingervein_recognition.m
@@ -15,7 +15,8 @@ end
 %% Variables
 GaborSigma = 5;
 dF = 2.5;
-F = 0.05;%(sqrt( log(2/pi))*(2^dF + 1)/(2^dF - 1)) / GaborSigma;
+F = 0.1014;%(sqrt( log(2/pi))*(2^dF + 1)/(2^dF - 1)) / GaborSigma;
+% should be optimal F according to Zhang and Yang.
 
 %% Processing
 % Show an image
@@ -35,7 +36,7 @@ subplot(2,1,2)
 
 K = 1 / (2*pi*GaborSigma^2);
 g = K * exp( -(x.^2 + y.^2)/(2*GaborSigma^2) ); % Nice looking Gaussian with low peak and wide distribution.
-G = exp( 2*pi*1i*F*( sqrt( x.^2 + y.^2 ) ) ); % Only the exponent looks as expected. USing the rest looks more like the traditional gabor.
+G = g * cos( 2*pi*1i*F*( sqrt( x.^2 + y.^2 ) ) ); % Only the exponent looks as expected. USing the rest looks more like the traditional gabor.
 
 figure
 subplot(1,3,1)
