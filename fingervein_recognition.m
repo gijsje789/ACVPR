2d100d18f65f6b5d97dabb19d4a2db0447cbfa4c
gijsje789 <gijsjaapdevries@hotmail.com>
2018-05-24 10:38:49 +0200

    Dilation to make the cropping more robust

--- fingervein_recognition.m
+++ fingervein_recognition.m
@@ -15,17 +15,19 @@ end
 blobAnalyzer = vision.BlobAnalysis('LabelMatrixOutputPort', true, ...
                                     'MinimumBlobArea', 25);
 %% Variables
+i = 1; % Which finger to take.
 
 %% Processing
 % Show an image
 origIm = figure;
 subplot(1,3,1)
-imshow(images{1}, [])
+imshow(images{i}, [])
 
-bw_thres = graythresh(images{1});
-bw_im = imbinarize(images{1}, bw_thres);
+bw_thres = graythresh(images{i});
+bw_im = imbinarize(images{i}, bw_thres);
+bw_im = imdilate(bw_im, strel('rectangle', [75 75])); %big dilate to compensate for crooked fingers.
 [area, centroid, ~, label] = blobAnalyzer(bw_im);
-[~, I] = max(area);
+[~, I] = max(area); % The blob with the largest area probably is the finger.
 
 subplot(1,3,2)
 imshow(bw_im);
@@ -33,6 +35,6 @@ hold on;
 plot(centroid(I,1), centroid(I,2), 'r*')
 hold off;
 
-cropped = images{1}(centroid(I,2)-80:centroid(I,2)+80, 1:end);
+cropped = images{i}(centroid(I,2)-80:centroid(I,2)+80, 1:end);
 subplot(1,3,3)
 imshow(cropped, []);
