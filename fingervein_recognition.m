380c59ee5a622ebc166f1ef79dfa65bae839132d
gijsje789 <gijsjaapdevries@hotmail.com>
2018-05-24 10:46:19 +0200

    Crop functionality in seperate function

--- fingervein_recognition.m
+++ fingervein_recognition.m
@@ -11,30 +11,19 @@ for ii=1:nfiles
    currentimage = imread(currentfilename);
    images{ii} = currentimage;
 end
-
-blobAnalyzer = vision.BlobAnalysis('LabelMatrixOutputPort', true, ...
-                                    'MinimumBlobArea', 25);
 %% Variables
-i = 1; % Which finger to take.
+% i = 1; % Which finger to take.
 
 %% Processing
 % Show an image
 origIm = figure;
-subplot(1,3,1)
-imshow(images{i}, [])
 
-bw_thres = graythresh(images{i});
-bw_im = imbinarize(images{i}, bw_thres);
-bw_im = imdilate(bw_im, strel('rectangle', [75 75])); %big dilate to compensate for crooked fingers.
-[area, centroid, ~, label] = blobAnalyzer(bw_im);
-[~, I] = max(area); % The blob with the largest area probably is the finger.
+for i=1:24
+    subplot(1,2,1)
+    imshow(images{i}, [])
 
-subplot(1,3,2)
-imshow(bw_im);
-hold on;
-plot(centroid(I,1), centroid(I,2), 'r*')
-hold off;
+    cropped = cropFingerVeinImage(images{i});
 
-cropped = images{i}(centroid(I,2)-80:centroid(I,2)+80, 1:end);
-subplot(1,3,3)
-imshow(cropped, []);
+    subplot(1,2,2)
+    imshow(cropped, []);
+end
