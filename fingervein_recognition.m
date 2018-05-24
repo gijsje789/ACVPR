6fa0b365f53662d42883df99a0d87e0ba2700391
gijsje789 <gijsjaapdevries@hotmail.com>
2018-05-24 15:39:07 +0200

    Gabor filter in seperate function

--- fingervein_recognition.m
+++ fingervein_recognition.m
@@ -15,32 +15,15 @@ end
 % i = 1; % Which finger to take.
 GaborSigma = 5;
 dF = 1.12; 
+F = (sqrt(log(2)/pi) * ((2^dF + 1)/(2^dF - 1)))/GaborSigma;
 % F = ((sqrt(2*log(2))/(2*pi)) * ((2^dF + 1)/(2^dF - 1)))/GaborSigma;
-F = ((1/pi) * sqrt( log(2)/2 ) * ((2^dF + 1)/(2^dF - 1)))/GaborSigma;
-x = -20:20;
-y = transpose(-20:20);    
+% F = ((1/pi) * sqrt( log(2)/2 ) * ((2^dF + 1)/(2^dF - 1)))/GaborSigma;
+   
 
 %% Processing
 % Show an image
 origIm = figure;
 gabor = figure;
-
-K = 1 / (2*pi*GaborSigma^2);
-g = K * exp( -(x.^2 + y.^2)/(2*GaborSigma^2) ); % Nice looking Gaussian with low peak and wide distribution.
-G = g .* exp( 2*pi*i*F*( sqrt( x.^2 + y.^2 ) ) ); % Only the exponent looks as expected. USing the rest looks more like the traditional gabor.
-
-subplot(2,3,1)
-surf(x,y,real(G));
-subplot(2,3,2)
-surf(x,y,imag(G));
-subplot(2,3,3)
-surf(x,y,abs(G));
-subplot(2,3,4)
-imshow(real(G), []);
-subplot(2,3,5)
-imshow(imag(G),[]);
-subplot(2,3,6)
-imshow(abs(G),[]);
 
 for i=1:24
     figure(origIm)
@@ -48,9 +31,9 @@ for i=1:24
     imshow(images{i}, [])
 
     cropped = cropFingerVeinImage(images{i});
-    
+    G = createGaborFilter(GaborSigma, F, 20);
     subplot(1,3,2)
-    imshow(transformed, []);
+    imshow(cropped, []);
     
     enhanced = conv2(cropped, abs(G));
     subplot(1,3,3)
