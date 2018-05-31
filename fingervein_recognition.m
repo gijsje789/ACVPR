815375e749054e6b57bfa28436e5750dbbba948e
s1880667 <m.h.coenen@student.utwente.nl>
2018-05-31 13:23:44 +0200

    fix mean

--- fingervein_recognition.m
+++ fingervein_recognition.m
@@ -30,62 +30,56 @@ imshow(images{1}, []);
 
 S= im2double(images{1});
 
-sigma = 5;
+sigma = 4;
 L = 2*ceil(sigma*3)+1;
 h = fspecial('gaussian', L, sigma);% create the PSF
 imfiltered = imfilter(S, h, 'replicate', 'conv'); % apply the filter
 
-S = mat2gray(imfiltered, [0 256]);
+S = imfiltered;%mat2gray(imfiltered, [0 256]);
 
 figure;
 imshow(S, []);
 
-%% maximum curvature method
-sigma = 5; % Parameter
-v_max_curvature = max_curvature(double(images{1}),1,sigma);
-
-% Binarise the vein image
-md = median(v_max_curvature(v_max_curvature>0));
-v_max_curvature_bin = v_max_curvature > md; 
-
-figure;
-subplot(2,1,1);
-imshow(v_max_curvature,[]);
-subplot(2,1,2);
-imshow(v_max_curvature_bin);
-
-%% repeated lines method
-
-fvr = ones(size(images{1}));
-
-veins = repeated_line(S, fvr, 3000, 1, 17);
-
-% Binarise the vein image
-md = median(veins(veins>0));
-v_repeated_line_bin = veins > md;
-
-se = strel('disk',1,0);
-v_repeated_line_bin = imerode(v_repeated_line_bin,se);
-
-figure; 
-subplot(2,1,1);
-imshow(veins,[]);
-subplot(2,1,2);
-imshow(v_repeated_line_bin);
+% %% maximum curvature method
+% sigma = 5; % Parameter
+% v_max_curvature = max_curvature(double(images{1}),1,sigma);
+% 
+% % Binarise the vein image
+% md = median(v_max_curvature(v_max_curvature>0));
+% v_max_curvature_bin = v_max_curvature > md; 
+% 
+% figure;
+% subplot(2,1,1);
+% imshow(v_max_curvature,[]);
+% subplot(2,1,2);
+% imshow(v_max_curvature_bin);
+% 
+% %% repeated lines method
+% 
+% fvr = ones(size(images{1}));
+% 
+% veins = repeated_line(S, fvr, 3000, 1, 17);
+% 
+% % Binarise the vein image
+% md = median(veins(veins>0));
+% v_repeated_line_bin = veins > md;
+% 
+% se = strel('disk',1,0);
+% v_repeated_line_bin = imerode(v_repeated_line_bin,se);
+% 
+% figure; 
+% subplot(2,1,1);
+% imshow(veins,[]);
+% subplot(2,1,2);
+% imshow(v_repeated_line_bin);
 
 %% Mean curvature method
-
-im = images{1};
-figure, imshow(im);
-fim=mat2gray(im);
-
-
 
 v_mean_curvature=mean_curvature(S);
 
 % Binarise the vein image
-md = median(v_mean_curvature(v_mean_curvature>0));
-v_mean_curvature_bin = v_max_curvature > md; 
+md = 0.03;
+v_mean_curvature_bin = v_mean_curvature > md; 
 
 figure;
 subplot(2,1,1);
