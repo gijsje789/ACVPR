cd61439066fadd8a89e75816e46eb12bcb54c8a6
Mark_B <m.n.bruijn@gmail.com>
2018-06-07 09:33:15 +0200

    folder update

--- own_functions - Copy/cropFingerVeinImageLee.m
+++
@@ -1,8 +0,0 @@
-function [cropped, region, edges] = cropFingerVeinImageLee(image)
-    
-    [region, edges] = lee_region(im2double(image), 4, 20);
-    avg_edges(1) = max(edges(1,:));
-    avg_edges(2) = min(edges(2,:));
-    cropped = image(avg_edges(1):avg_edges(2), 1:end);
-
-end
