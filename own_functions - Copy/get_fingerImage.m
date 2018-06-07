70efa88bc4491f0e5adf414b2da2c5016a3b58c7
Mark_B <m.n.bruijn@gmail.com>
2018-06-07 09:31:25 +0200

    test push

---
+++ own_functions - Copy/get_fingerImage.m
@@ -0,0 +1,19 @@
+function [im] = get_fingerImage(imageSet, person, finger, number)
+% Returns a specific image using data indices person,finger,number from a
+% dataset
+
+% Parameters:
+%  person   - person number -- integer
+%  finger   - finger number -- integer
+%  number   - photo  number -- integer
+
+% Returns:
+%  im       - image of the person, their finger, the corresponding photo
+
+imageSet = imageSet(cell2mat(imageSet(:,2)) == person,:,:,:);
+imageSet = imageSet(cell2mat(imageSet(:,3)) == finger,:,:,:);
+imageSet = imageSet(cell2mat(imageSet(:,4)) == number,:,:,:);
+
+im = cell2mat(imageSet(1,1));
+
+
