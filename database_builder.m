cdb7d2675917b352412a2b92a26a94d5ac0b5241
gijsje789 <gijsjaapdevries@hotmail.com>
2018-06-14 17:47:56 +0200

    cleaned database builder, MEC to own function

--- database_builder.m
+++ database_builder.m
@@ -11,7 +11,7 @@ FINGER_COUNT = 6;               % max 6
 FINGER_PHOTO_COUNT = 4;         % max 4
 RL_SKEL = true;            % Enable repeated line tracking
 MAC_SKEL = true;            % Enable MAC
-MEC_SKEL = false;           % Enable MEC
+MEC_SKEL = true;           % Enable MEC
 LBP_EN = true;             % Enable LBP
 
 
@@ -42,64 +42,14 @@ for person = 1:PERSON_COUNT
             end
             %% build MEC skeleton
             if MEC_SKEL
-                % Tested using enhance; did not improve results. 
-                %ima = adapthisteq(image);
-                %image = enhance_finger(current_source_img);
-                image = imresize(im2double(current_source_img), 0.5);
-                %image = current_source_img;
-
-                mask_height=4; % Height of the mask
-                mask_width=20; % Width of the mask
-                [fvr, edges] = lee_region(image,mask_height,mask_width);
-                x = 1: 1 : length(edges(2,:));
-
-                [m,n] = size(image);
-
-                for c = 1 : n
-                    for r = 1 : m
-                        if r > edges(1,c)
-                           image(1:r-1,c) = 0; 
-                           break
-                        end
-                    end
-                end
-
-                for c = 1 : n
-                    for r = 1 : m
-                        if r > edges(2,c)
-                           image(r:m,c) = 0; 
-                           break
-                        end
-                    end
-                end
-
-                % gaussian filter
-
-                S= im2double(image);
-
-                sigma = 3.2;
-                L = 2*ceil(sigma*3)+1;
-                h = fspecial('gaussian', L, sigma);% create the PSF
-                imfiltered = imfilter(S, h, 'replicate', 'conv'); % apply the filter
-
-                S = imfiltered;%mat2gray(imfiltered, [0 256]);
-
-                % Mean curvature method
-
-                v_mean_curvature = mean_curvature(S);
-
-                % Binarise the vein image
-                md = 0.01;
-                img_mec_bin = v_mean_curvature > md; 
-
-
-                bw1 = filledgegaps(img_mec_bin, 7);
-                img_mec_skeleton  = bwareaopen(bw1,10);
+                tic
+                img_mec_skeleton = MECskeletonize(img);
+                toc
             end
             %% find LBP features
             if LBP_EN
                 tic
-                lbp_info = createLBPofSkel(img_enhanced_mac, branch_array_mac);
+                lbp_info = createLBPofSkel(current_source_img, branch_array_mac);
                 toc
             end
             %% fill database entry
@@ -128,7 +78,7 @@ for person = 1:PERSON_COUNT
             
             if MEC_SKEL
                 data{db_counter,7} = img_mec_skeleton;         % MEC skeletonized
-                data{db_counter,10} = branch_array_mec;        % branchpoint array MEC
+%                 data{db_counter,10} = branch_array_mec;        % branchpoint array MEC
             end
             
             if LBP_EN
--- database_match_finder.m
+++ database_match_finder.m
@@ -19,12 +19,12 @@ for compare = 1:data_count
     finger_reference = data{compare,3};                      % finger number
     number_reference = data{compare,4};                      % photo number
     img_rl_bin_reference = data{compare,5};                  % RL binary
-%     img_mac_bin_reference = data{compare,6};                 % MAC binary
-    %img_mec_skeleton_reference = data{compare,7};          % MEC skeletonized
+    img_mac_bin_reference = data{compare,6};                 % MAC binary
+    img_mec_skeleton_reference = data{compare,7};          % MEC skeletonized
     branch_array_rl_reference = data{compare,8};             % branchpoint array RL
-%     branch_array_mac_reference = data{compare,9};            % branchpoint array MAC
-    %branch_array_mec_reference = data{compare,10};         % branchpoint array MEC
-%     lbp_info_reference = data{compare,11};                   % local binary pattern
+    branch_array_mac_reference = data{compare,9};            % branchpoint array MAC
+    branch_array_mec_reference = data{compare,10};         % branchpoint array MEC
+    lbp_info_reference = data{compare,11};                   % local binary pattern
     
     for compare_with = 1:data_count
         
@@ -34,18 +34,17 @@ for compare = 1:data_count
         finger = data{compare_with,3};                      % finger number
         number = data{compare_with,4};                      % photo number
         img_rl_bin = data{compare_with,5};                  % RL binary
-%         img_mac_bin = data{compare_with,6};                 % MAC binary
-        %img_mec_skeleton = data{compare_with,7};            % MEC skeletonized
+        img_mac_bin = data{compare_with,6};                 % MAC binary
+        img_mec_skeleton = data{compare_with,7};            % MEC skeletonized
         branch_array_rl = data{compare_with,8};             % branchpoint array RL
-%         branch_array_mac = data{compare_with,9};            % branchpoint array MAC
-        %branch_array_mec = data{compare_with,10};           % branchpoint array MEC
-%         lbp_info = data{compare_with,11};                   % local binary pattern
+        branch_array_mac = data{compare_with,9};            % branchpoint array MAC
+        branch_array_mec = data{compare_with,10};           % branchpoint array MEC
+        lbp_info = data{compare_with,11};                   % local binary pattern
         
         %% ================== select matching method =============================
         %full_match_percentage = lbp_matching(lbp_info, lbp_info_reference);
-        full_match_percentage = template_matching(img_rl_bin_reference, img_rl_bin); %6%
-%         full_match_percentage = template_matching(img_mac_bin_reference,
-%         img_mac_bin); %enhanced 2% anders rond 0%
+%         full_match_percentage = template_matching(img_rl_bin_reference, img_rl_bin); %6%
+        full_match_percentage = template_matching(img_mac_bin_reference, img_mac_bin); %enhanced 2% anders rond 0%
 %         [~, error, ~, full_match_percentage] = matchLBPfeatures(lbp_info, lbp_info_reference);
 %         [index, full_match_percentage] = matchFeatures(lbp_info, lbp_info_reference);
 %         full_match_percentage = sum(full_match_percentage);
