86e85685beb5cae81f5b94d540e853cc4edc5cc1
gijsje789 <gijsjaapdevries@hotmail.com>
2018-06-14 16:53:40 +0200

    removed loop and replaced by simpler, moved functionality into subfunction

--- database_builder.m
+++ database_builder.m
@@ -34,97 +34,48 @@ for person = 1:PERSON_COUNT
             [fvr, edges] = lee_region(img_gauss,mask_height,mask_width);
             
             [m,n] = size(img);
-            
-            for c = 1 : n
-                for r = 1 : m
-                    if r > edges(1,c)
-                        img_gauss(1:r-1,c) = 0;
-                        break
-                    end
-                end
+
+            for col = 1:size(edges,2)
+                img_gauss(1:edges(1,col), col) = 0;
+                img_gauss(edges(2,col):end, col) = 0;
             end
+
+            [img_rl_bin, branch_array_rl] = repeatedLineTracking(img_gauss, fvr);
+
             
-            for c = 1 : n
-                for r = 1 : m
-                    if r > edges(2,c)
-                        img_gauss(r:m,c) = 0;
-                        break
-                    end
-                end
-            end
-            
-            % repeated lines method
-            %fvr = ones(size(im));
-            veins = repeated_line(img_gauss, fvr, 3000, 1, 17);
-            
-            % binarize the vein image
-            md = median(veins(veins>0));
-            v_repeated_line_bin = veins > md;
-            
-            % clean and fill (correct isolated black and white pixels)
-            img_rl_clean = bwmorph(v_repeated_line_bin,'clean');
-            img_rl_fill = bwmorph(img_rl_clean,'fill');
-            
-            % for export to database
-            img_rl_bin = img_rl_fill;
-            
-            % skeletonize first time
-            img_rl_skel = bwmorph(img_rl_fill,'skel',inf);
-            
-            % open filter image
-            img_rl_open = bwareaopen(img_rl_skel, 10);
-            
-            % fill gaps smaller than 7 pixels
-            img_filledgaps = filledgegaps(img_rl_open, 7);
-            
-            % remove dead ends shorter than 10 pixels
-            skelD = removeDeadEnds(img_filledgaps, 10);
-            
-            % clean and fill (correct isolated black and white pixels)
-            img_rl_clean = bwmorph(skelD,'clean');
-            img_rl_result = bwmorph(img_rl_clean,'fill');
-            
-            % skeletonize again to optimize branchpoint detection
-            img_rl_skeleton = bwmorph(img_rl_result,'skel',inf);
-            
-            % find branchpoints remaining and put in array
-            bw1br = bwmorph(img_rl_skeleton, 'branchpoints');
-            [i,j] = find(bw1br);
-            branch_array_rl = [j,i];
-            
-            %% build MAC skeleton
-            
-            %img_mac = imresize(img_rl, 0.5);
-            
-            img_enhanced_mac = img;
-            
-            % find Lee regions (finger region)
-            fvr = lee_region(img,4,40);
-            
-            % extract veins using maximum curvature method
-            v_max_curvature = miura_max_curvature(img,fvr,3);
-            
-            % binarize the vein image
-            md = median(v_max_curvature(v_max_curvature>0));
-            v_max_curvature_bin = v_max_curvature > md;
-            
-            img_mac_bin = v_max_curvature_bin;
-            
-            % skeletonize and fill gaps
-            bw1 = filledgegaps(v_max_curvature_bin, 7);
-            img_mac_skeleton  = bwareaopen(bw1,5);
-            
-            % find branchpoints remaining and put in array
-            bw1br = bwmorph(img_mac_skeleton, 'branchpoints');
-            [i,j] = find(bw1br);
-            branch_array_mac = [j,i];
-            
-            %% build MEC skeleton
-            
-            %% find LBP features
-            
-            lbp_info = createLBPofSkel(img_enhanced_mac, branch_array_mac);
-            %lbp_info = createLBPofSkel(img_enhanced_rl, branch_array_rl);
+%             %% build MAC skeleton
+%             
+%             %img_mac = imresize(img_rl, 0.5);
+%             
+%             img_enhanced_mac = img;
+%             
+%             % find Lee regions (finger region)
+%             fvr = lee_region(img,4,40);
+%             
+%             % extract veins using maximum curvature method
+%             v_max_curvature = miura_max_curvature(img,fvr,3);
+%             
+%             % binarize the vein image
+%             md = median(v_max_curvature(v_max_curvature>0));
+%             v_max_curvature_bin = v_max_curvature > md;
+%             
+%             img_mac_bin = v_max_curvature_bin;
+%             
+%             % skeletonize and fill gaps
+%             bw1 = filledgegaps(v_max_curvature_bin, 7);
+%             img_mac_skeleton  = bwareaopen(bw1,5);
+%             
+%             % find branchpoints remaining and put in array
+%             bw1br = bwmorph(img_mac_skeleton, 'branchpoints');
+%             [i,j] = find(bw1br);
+%             branch_array_mac = [j,i];
+%             
+%             %% build MEC skeleton
+%             
+%             %% find LBP features
+%             
+%             lbp_info = createLBPofSkel(img_enhanced_mac, branch_array_mac);
+%             %lbp_info = createLBPofSkel(img_enhanced_rl, branch_array_rl);
             
             %% fill database entry
             data{db_counter,1} = current_source_img;       % non-cropped finger image
