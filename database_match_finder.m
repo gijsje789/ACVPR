aa9b2cc22b17a12a41a92a92c30978046e177d19
Mark_B <m.n.bruijn@gmail.com>
2018-06-14 23:41:41 +0200

    verified all working, improved comments

--- database_match_finder.m
+++ database_match_finder.m
@@ -18,34 +18,34 @@ m_counter = 1;
 
 for compare = 1:data_count
     % read all data for first database entry
-    current_source_img_reference = data{compare,1};          % non-cropped finger image
+    current_source_img_reference = data{compare,1};          % original finger image
     person_reference = data{compare,2};                      % person number
     finger_reference = data{compare,3};                      % finger number
     number_reference = data{compare,4};                      % photo number
     img_rl_bin_reference = data{compare,5};                  % RL binary
     img_mac_bin_reference = data{compare,6};                 % MAC binary
-    img_mec_bin_reference = data{compare,7};                 % MEC skeletonized
+    img_mec_bin_reference = data{compare,7};                 % MEC binary
     branch_array_rl_reference = data{compare,8};             % branchpoint array RL
     branch_array_mac_reference = data{compare,9};            % branchpoint array MAC
     branch_array_mec_reference = data{compare,10};           % branchpoint array MEC
     lbp_info_reference = data{compare,11};                   % local binary pattern
-    img_mac_gray_reference = data{compare,12};               % local binary pattern
+    img_mac_gray_reference = data{compare,12};               % gray MAC image for LBP
     
     for compare_with = 1:data_count
         
         % read all data for first database entry
-        current_source_img = data{compare_with,1};          % non-cropped finger image
+        current_source_img = data{compare_with,1};          % original finger image
         person = data{compare_with,2};                      % person number
         finger = data{compare_with,3};                      % finger number
         number = data{compare_with,4};                      % photo number
         img_rl_bin = data{compare_with,5};                  % RL binary
         img_mac_bin = data{compare_with,6};                 % MAC binary
-        img_mec_bin = data{compare_with,7};                 % MEC skeletonized
+        img_mec_bin = data{compare_with,7};                 % MEC binary
         branch_array_rl = data{compare_with,8};             % branchpoint array RL
         branch_array_mac = data{compare_with,9};            % branchpoint array MAC
         branch_array_mec = data{compare_with,10};           % branchpoint array MEC
         lbp_info = data{compare_with,11};                   % local binary pattern
-        img_mac_gray = data{compare_with,12};                   % local binary pattern
+        img_mac_gray = data{compare_with,12};               % gray MAC image for LBP
         
         % matching method specific actions
         if strcmp(test_method,'RL')
